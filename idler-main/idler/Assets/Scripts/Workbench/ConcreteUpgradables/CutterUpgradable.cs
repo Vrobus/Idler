6ab399b26fc9d49318d00cbf9e11204d3d3502d1
cannedenuum <cannedenuum@hotmail.com>
2025-06-01 21:58:50 +0300

    feat: Cutter upgradable increments every resource after a specified interval

--- idler-main/idler/Assets/Scripts/Workbench/ConcreteUpgradables/CutterUpgradable.cs
+++ idler-main/idler/Assets/Scripts/Workbench/ConcreteUpgradables/CutterUpgradable.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class CutterUpgradable : WorkbenchUpgradable
 {
+    [SerializeField] private float[] incrementIntervalLevels;
+
+    private Coroutine incrementResourcesRoutine;
+
     protected override void IncrementStats()
     {
+        if (incrementResourcesRoutine != null) StopCoroutine(incrementResourcesRoutine);
+        incrementResourcesRoutine = StartCoroutine(IncrementResourcesRoutine());
+    }
 
+    private IEnumerator IncrementResourcesRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(incrementIntervalLevels[level - 2]);
+            
+            foreach (var resource in resourceWallet.Resources)
+            {
+                resource.Amount++;
+            }
+        }
     }
 }
